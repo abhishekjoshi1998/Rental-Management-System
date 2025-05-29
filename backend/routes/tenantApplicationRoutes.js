c4c22e92aad53b8059c14f08ebedb7baa0b77bd5
abhishekjoshi1998 <abhishekjoshi9997@gmail.com>
2025-05-29 13:59:44 +0530

    tenent route

---
+++ backend/routes/tenantApplicationRoutes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const {
+    submitApplication,
+    getApplications,
+    getApplicationById,
+    updateApplicationStatus,
+    uploadApplicationDocument,
+    deleteApplicationDocument,
+    initiateApplicationBackgroundCheck,
+    getApplicationBackgroundCheckStatus
+} = require('../controllers/tenantApplicationController');
+const { protect, authorize } = require('../middlewares/authMiddleware');
+const upload = require('../middlewares/uploadMiddleware');
+
+const router = express.Router();
+
+const applicationUploads = upload.fields([
+    { name: 'identification_doc', maxCount: 2 },
+    { name: 'income_proof_doc', maxCount: 3 },
+    { name: 'reference_letter_doc', maxCount: 3 },
+    { name: 'other_doc', maxCount: 5 }
+]);
+
+
+router.route('/')
+    .post(protect, authorize('tenant'), applicationUploads, submitApplication)
+    .get(protect, authorize('landlord', 'property_manager', 'admin', 'tenant'), getApplications);
+
+router.route('/:id')
+    .get(protect, authorize('landlord', 'property_manager', 'admin', 'tenant'), getApplicationById);
+
+router.put('/:id/status', protect, authorize('landlord', 'property_manager', 'admin'), updateApplicationStatus);
+
+router.post('/:id/documents', protect, authorize('tenant', 'admin', 'property_manager'), upload.array('documents', 5), uploadApplicationDocument);
+router.delete('/:applicationId/documents/:documentId', protect, authorize('tenant', 'admin', 'property_manager'), deleteApplicationDocument);
+
+router.post('/:id/background-check', protect, authorize('landlord', 'property_manager', 'admin'), initiateApplicationBackgroundCheck);
+router.get('/:id/background-check/status', protect, authorize('landlord', 'property_manager', 'admin'), getApplicationBackgroundCheckStatus);
+
+
+module.exports = router;
