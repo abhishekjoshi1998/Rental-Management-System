8845012766a228952960768bd10161a1217a08e6
abhishekjoshi1998 <abhishekjoshi9997@gmail.com>
2025-05-31 18:14:03 +0530

    update

---
+++ frontend/vite-rentelo/src/components/Common/AlertMessage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const AlertMessage = ({ type, message }) => {
+  const baseClass = 'alert-message';
+  let typeClass = '';
+
+  switch (type) {
+    case 'error':
+      typeClass = 'error-message';
+      break;
+    case 'success':
+      typeClass = 'success-message';
+      break;
+    case 'info':
+      typeClass = 'info-message';
+      break;
+    case 'warning':
+      typeClass = 'warning-message';
+      break;
+    default:
+      typeClass = 'info-message';
+  }
+
+  if (!message) {
+    return null;
+  }
+
+  return (
+    <div className={`${baseClass} ${typeClass}`}>
+      {message}
+    </div>
+  );
+};
+
+export default AlertMessage;
